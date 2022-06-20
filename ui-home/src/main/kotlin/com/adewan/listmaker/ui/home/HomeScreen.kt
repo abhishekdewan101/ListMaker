aa4cd29b6c674bdfda0e6818d20e5f789c7dbfd2
Abhishek Dewan <abhishekdewan2802@gmail.com>
2022-06-19 17:34:03 -0700

    Removing the extra top bar to make some UI designs.

--- ui-home/src/main/kotlin/com/adewan/listmaker/ui/home/HomeScreen.kt
+++ ui-home/src/main/kotlin/com/adewan/listmaker/ui/home/HomeScreen.kt
@@ -1,45 +1,17 @@
 package com.adewan.listmaker.ui.home
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import com.adewan.listmaker.common.navigation.AppNavigator
 import com.adewan.listmaker.ui.common.ThemedContainer
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navigator: AppNavigator) {
     ThemedContainer {
-        Scaffold(topBar = { TopBar(appNavigator = navigator) }) {
+        Scaffold {
 
         }
-    }
-
-}
-
-@Composable
-private fun TopBar(appNavigator: AppNavigator) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 10.dp, vertical = 20.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(
-            "ListMaker",
-            style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
-        )
     }
 }
