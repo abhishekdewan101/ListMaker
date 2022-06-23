147a23ac9d1f6b38f590c11b1037bd3f014aa242
Abhishek Dewan <abhishekdewan2802@gmail.com>
2022-06-22 21:57:09 -0700

    Cool now everything works !

--- ui-home/src/main/kotlin/com/adewan/listmaker/ui/home/HomeScreenViewModel.kt
+++ ui-home/src/main/kotlin/com/adewan/listmaker/ui/home/HomeScreenViewModel.kt
@@ -2,13 +2,9 @@ package com.adewan.listmaker.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.adewan.listmaker.models.ListType
 import com.adewan.listmaker.repositories.ListRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import java.util.UUID
 import javax.inject.Inject
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 @HiltViewModel
@@ -16,10 +12,9 @@ class HomeScreenViewModel @Inject constructor(private val listRepository: ListRe
     ViewModel() {
     init {
         viewModelScope.launch {
-            listRepository.getAllLists().onEach {
-                println("Lists -> $it")
-            }.collect()
+            listRepository.getAllLists().collect {
+                println("Lists Count -> ${it.size}")
+            }
         }
-        listRepository.addList(UUID.randomUUID(), "First List", ListType.GAME)
     }
 }
